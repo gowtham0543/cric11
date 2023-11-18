4ca95c1d16c32ec65d84aa1b30d23ca3d94783d3
gowtham0543 <139748746+gowtham0543@users.noreply.github.com>
2023-11-18 13:39:30 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -1,11 +1,13 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
-    profile_picture = models.ImageField(null=True, blank=True)
-    bio = models.TextField()
-    date_of_birth = models.DateField(null=True, blank=True)
+# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length=3)
+    color = models.CharField(max_length=255)
+    flag = models.ImageField(max_length=255)
 
     def __str__(self):
-        return self.username
+        return self.name
+
